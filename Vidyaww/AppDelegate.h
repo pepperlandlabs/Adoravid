99fe2f0cfa3bbca3e4e0528ea813a342e91e97b2
tywhite <tyson.v.white@gmail.com>
2013-09-29 17:04:03 -0700

    Initial Commit

---
+++ Vidyaww/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Vidyaww
+//
+//  Created by Tyson White on 9/29/13.
+//  Copyright (c) 2013 Tyson White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Vidyaww/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Vidyaww
+//
+//  Created by Tyson White on 9/29/13.
+//  Copyright (c) 2013 Tyson White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
